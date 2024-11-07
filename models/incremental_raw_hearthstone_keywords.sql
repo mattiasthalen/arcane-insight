6de2ccfdc554c31525b2cd716e66ab300168ad5b
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-07 23:38:34 +0100

    Init sqlmesh

---
+++ models/incremental_raw_hearthstone_keywords.sql
@@ -0,0 +1,30 @@
+MODEL (
+  name battle_net_sqlmesh.incremental_raw_hearthstone_keywords,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column _dlt_load_time,
+  ),
+  columns (
+    id BIGINT,
+    slug TEXT,
+    name TEXT,
+    ref_text TEXT,
+    text TEXT,
+    _dlt_load_id TEXT,
+    _dlt_id TEXT,
+    _dlt_load_time TIMESTAMP
+  ),
+);
+
+SELECT
+  id,
+  slug,
+  name,
+  ref_text,
+  text,
+  _dlt_load_id,
+  _dlt_id,
+  TO_TIMESTAMP(CAST(_dlt_load_id AS DOUBLE)) as _dlt_load_time
+FROM
+  battle_net.raw_hearthstone_keywords
+WHERE
+  TO_TIMESTAMP(CAST(_dlt_load_id AS DOUBLE)) BETWEEN @start_ds AND @end_ds
---
+++ models/incremental_raw_hearthstone_set_groups.sql
@@ -0,0 +1,34 @@
+MODEL (
+  name battle_net_sqlmesh.incremental_raw_hearthstone_set_groups,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column _dlt_load_time,
+  ),
+  columns (
+    slug TEXT,
+    year BIGINT,
+    svg TEXT,
+    name TEXT,
+    standard BOOLEAN,
+    icon TEXT,
+    _dlt_load_id TEXT,
+    _dlt_id TEXT,
+    year_range TEXT,
+    _dlt_load_time TIMESTAMP
+  ),
+);
+
+SELECT
+  slug,
+  year,
+  svg,
+  name,
+  standard,
+  icon,
+  _dlt_load_id,
+  _dlt_id,
+  year_range,
+  TO_TIMESTAMP(CAST(_dlt_load_id AS DOUBLE)) as _dlt_load_time
+FROM
+  battle_net.raw_hearthstone_set_groups
+WHERE
+  TO_TIMESTAMP(CAST(_dlt_load_id AS DOUBLE)) BETWEEN @start_ds AND @end_ds
