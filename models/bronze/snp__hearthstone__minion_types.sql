f1a0c61375dc8a0a17844d7ed176d59eacb5a8ce
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-11 18:01:54 +0100

    Format SQL

--- models/bronze/snp__hearthstone__minion_types.sql
+++ models/bronze/snp__hearthstone__minion_types.sql
@@ -7,13 +7,13 @@ MODEL (
 );
 
 SELECT
-    *,
-    @generate_surrogate_key__sha_256(
-        @star_v2(
-            relation := raw__hearthstone__minion_types,
-            exclude := _sqlmesh__extracted_at,
-            select_only := TRUE
-        )
-    ) AS _sqlmesh__hash_diff,
-    @execution_ts::TIMESTAMP AS _sqlmesh__loaded_at
+  *,
+  @generate_surrogate_key__sha_256(
+    @star_v2(
+      relation := raw__hearthstone__minion_types,
+      exclude := _sqlmesh__extracted_at,
+      select_only := TRUE
+    )
+  ) AS _sqlmesh__hash_diff,
+  @execution_ts::TIMESTAMP AS _sqlmesh__loaded_at
 FROM bronze.raw__hearthstone__minion_types
