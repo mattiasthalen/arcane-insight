c4b0f5178562181040633df276d9de367f42a989
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-14 08:43:37 +0100

    Move load_dotenv() inside the execute function of python models

--- models/bronze/raw/raw__hearthstone__cards.py
+++ models/bronze/raw/raw__hearthstone__cards.py
@@ -9,8 +9,6 @@ from dotenv import load_dotenv
 from sqlmesh import ExecutionContext, model
 from sqlmesh.core.model.kind import ModelKindName
 
-load_dotenv()
-
 columns={
     "armor": "text",
     "artistName": "text",
@@ -66,6 +64,8 @@ def execute(
 ) -> t.Generator[pd.DataFrame, None, None]:
     
     # Authorization
+    load_dotenv()
+    
     token_url = "https://oauth.battle.net/token"
     auth_dict = {
         "grant_type": "client_credentials",
