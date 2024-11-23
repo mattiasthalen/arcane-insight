4227f5f70377b5b559fe7a9857654bad2b0cd2ba
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-23 21:17:49 +0100

    Update example for data_vault__staging

--- macros/data_vault.py
+++ macros/data_vault.py
@@ -51,43 +51,32 @@ def data_vault__staging(
         >>> from sqlglot import parse_one
         >>> from sqlmesh.core.macros import MacroEvaluator
         >>> sql = \"\"\"
-        ... @DATA_VAULT__STAGING(
-        ...     source := bronze.snapshot.snp__hearthstone__cards,
-        ...     lookup_data := (
-        ...       class_slug := (
-        ...         lookup_table := bronze.snapshot.snp__hearthstone__classes,
-        ...         lookup_column := slug,
-        ...         left_column := classId,
-        ...         right_column := id
-        ...       ),
-        ...       type_slug := (
-        ...         lookup_table := bronze.snapshot.snp__hearthstone__types,
-        ...         lookup_column := slug,
-        ...         left_column := cardTypeId,
-        ...         right_column := id
-        ...       )
-        ...     ),
-        ...     derived_columns := (
-        ...       card_bk := id || '|' || slug,
-        ...       class_bk := class_slug,
-        ...       type_bk := type_slug
-        ...     ),
-        ...     hashes := (
-        ...       card_hk := card_bk,
-        ...       class_hk := class_bk,
-        ...       type_hk := type_bk,
-        ...       card_hk__class_hk := (card_bk, class_bk),
-        ...       card_hk__type_hk := (card_bk, type_bk),
-        ...       card__pit_hk := (card_bk, _sqlmesh__valid_from)
-        ...     ),
-        ...     source_system := 'hearthstone',
-        ...     loaded_at := _sqlmesh__loaded_at,
-        ...     valid_from := _sqlmesh__valid_from,
-        ...     valid_to := _sqlmesh__valid_to,
-        ...     generate_ghost_record := TRUE,
-        ...     hash_function := 'SHA256'
+        ... @data_vault__staging(
+        ...   source := bronze.snapshot.snp__hearthstone__cards,
+        ...   lookup_data := [
+        ...     class_bk := (
+        ...       lookup_table := bronze.snapshot.snp__hearthstone__classes,
+        ...       lookup_column := slug,
+        ...       left_column := classId,
+        ...       right_column := id
+        ...     )
+        ...   ],
+        ...   derived_columns := [
+        ...     card_bk := slug
+        ...   ],
+        ...   hashes := [
+        ...     card_hk := card_bk,
+        ...     class_hk := class_bk,
+        ...     card_hk__class_hk := (card_bk, class_bk),
+        ...     card_pit_hk := (card_bk, _sqlmesh__valid_from)
+        ...   ],
+        ...   source_system := 'hearthstone',
+        ...   loaded_at := _sqlmesh__loaded_at,
+        ...   valid_from := _sqlmesh__valid_from,
+        ...   valid_to := _sqlmesh__valid_to,
+        ...   generate_ghost_record := TRUE,
+        ...   hash_function := 'SHA256'
         ... )
-
     """
     
     # Define final query
