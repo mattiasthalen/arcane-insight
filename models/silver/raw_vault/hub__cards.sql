dd99452369c7bf28ad9de94ae16287608be51032
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-27 22:10:25 +0100

    Add hubs

---
+++ models/silver/raw_vault/hub__cards.sql
@@ -0,0 +1,41 @@
+/* Data vault hub model for the Hearthstone cards */
+MODEL (
+  name silver.raw_vault.raw__hub__cards,
+  kind FULL
+);
+
+WITH business_keys AS (
+  SELECT
+    0 AS source,
+    card_bk,
+    hash_key__card_bk,
+    _sqlmesh__record_source,
+    _sqlmesh__loaded_at
+  FROM silver.staging.dv_stg__hearthstone__cards
+  UNION ALL
+  SELECT
+    1 AS source,
+    id::TEXT AS card_bk,
+    @generate_surrogate_key(id::TEXT, hash_function := 'SHA256') AS hash_key__card_bk,
+    _sqlmesh__record_source,
+    _sqlmesh__loaded_at
+  FROM silver.staging.dv_stg__hearthstone__cards
+  UNION ALL
+  SELECT
+    2 AS source,
+    card_bk,
+    hash_key__card_bk,
+    _sqlmesh__record_source,
+    _sqlmesh__loaded_at
+  FROM silver.staging.dv_stg__hearthstone__classes
+), deduplicated AS (
+  SELECT
+    *
+    EXCLUDE (source)
+  FROM business_keys
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY card_bk ORDER BY source, _sqlmesh__loaded_at) = 1
+)
+SELECT
+  *
+FROM deduplicated
---
+++ models/silver/raw_vault/hub__classes.sql
@@ -0,0 +1,41 @@
+/* Data vault hub model for the Hearthstone classes */
+MODEL (
+  name silver.raw_vault.raw__hub__classes,
+  kind FULL
+);
+
+WITH business_keys AS (
+  SELECT
+    0 AS source,
+    class_bk,
+    hash_key__class_bk,
+    _sqlmesh__record_source,
+    _sqlmesh__loaded_at
+  FROM silver.staging.dv_stg__hearthstone__classes
+  UNION ALL
+  SELECT
+    1 AS source,
+    id::TEXT AS class_bk,
+    @generate_surrogate_key(id::TEXT, hash_function := 'SHA256') AS hash_key__class_bk,
+    _sqlmesh__record_source,
+    _sqlmesh__loaded_at
+  FROM silver.staging.dv_stg__hearthstone__classes
+  UNION ALL
+  SELECT
+      2 AS source,
+      class_bk,
+      hash_key__class_bk,
+      _sqlmesh__record_source,
+      _sqlmesh__loaded_at
+    FROM silver.staging.dv_stg__hearthstone__cards
+), deduplicated AS (
+  SELECT
+    *
+    EXCLUDE (source)
+  FROM business_keys
+  QUALIFY
+    ROW_NUMBER() OVER (PARTITION BY class_bk ORDER BY source, _sqlmesh__loaded_at) = 1
+)
+SELECT
+  *
+FROM deduplicated
