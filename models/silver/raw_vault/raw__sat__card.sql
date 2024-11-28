d882c6bd43a966b0d1cc1147e1b61d4e2eeb9643
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-28 08:59:18 +0100

    Add satellites

---
+++ models/silver/raw_vault/raw__sat__card.sql
@@ -0,0 +1,38 @@
+MODEL (
+  name silver.raw_vault.raw__sat__card,
+  kind SCD_TYPE_2_BY_COLUMN (
+    unique_key card_bk,
+    columns [_sqlmesh__hash_diff],
+    valid_to_name _sqlmesh__valid_to,
+    valid_from_name _sqlmesh__valid_from
+  ),
+  allow_partials TRUE
+);
+
+SELECT
+  card_bk,
+  id,
+  armor,
+  artistName,
+  attack,
+  bannedFromSideboard,
+  collectible,
+  cropImage,
+  durability,
+  flavorText,
+  health,
+  image,
+  imageGold,
+  isZilliaxCosmeticModule,
+  isZilliaxFunctionalModule,
+  manaCost,
+  maxSideboardCards,
+  name,
+  runeCost,
+  slug,
+  text,
+  _sqlmesh__hash_diff,
+  _sqlmesh__record_source,
+  _sqlmesh__extracted_at,
+  _sqlmesh__loaded_at
+FROM silver.staging.dv_stg__hearthstone__cards
---
+++ models/silver/raw_vault/raw__sat__class.sql
@@ -0,0 +1,21 @@
+MODEL (
+  name silver.raw_vault.raw__sat__class,
+  kind SCD_TYPE_2_BY_COLUMN (
+    unique_key class_bk,
+    columns [_sqlmesh__hash_diff],
+    valid_to_name _sqlmesh__valid_to,
+    valid_from_name _sqlmesh__valid_from
+  ),
+  allow_partials TRUE
+);
+
+SELECT
+  class_bk,
+  slug,
+  id,
+  name,
+  _sqlmesh__hash_diff,
+  _sqlmesh__record_source,
+  _sqlmesh__extracted_at,
+  _sqlmesh__loaded_at
+FROM silver.staging.dv_stg__hearthstone__cards
