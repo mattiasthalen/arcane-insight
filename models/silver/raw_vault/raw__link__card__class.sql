18845ac6a35b11c5ffa376f0cf10b7cb428ba107
Mattias Thalén <bitter-polders0x@icloud.com>
2024-11-28 09:15:08 +0100

    Format SQL

--- models/silver/raw_vault/raw__link__card__class.sql
+++ models/silver/raw_vault/raw__link__card__class.sql
@@ -13,11 +13,10 @@ MODEL (
 );
 
 SELECT
-    hash_key__card_bk__class_bk
-,   hash_key__card_bk
-,   hash_key__class_bk
-,   _sqlmesh__record_source
-,   _sqlmesh__extracted_at
-,   _sqlmesh__loaded_at
-
+  hash_key__card_bk__class_bk,
+  hash_key__card_bk,
+  hash_key__class_bk,
+  _sqlmesh__record_source,
+  _sqlmesh__extracted_at,
+  _sqlmesh__loaded_at
 FROM silver.staging.dv_stg__hearthstone__cards
