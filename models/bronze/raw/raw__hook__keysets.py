529853e10e82bb4b2ae0c77e79549242ceb068c6
Mattias Thalén <bitter-polders0x@icloud.com>
2025-01-15 08:50:06 +0100

    Add keysets elt

---
+++ models/bronze/raw/raw__hook__keysets.py
@@ -0,0 +1,54 @@
+import os
+import pandas as pd
+import typing as t
+import yaml
+
+from datetime import datetime
+from sqlmesh import ExecutionContext, model
+from sqlmesh.core.model.kind import ModelKindName
+
+@model(
+    name='bronze.raw.raw__hook__keysets',
+    description='Extract & load model for the keysets in hook yaml.',
+    kind=dict(
+        name=ModelKindName.FULL,
+    ),
+    columns={
+        "id": "int",
+        "source_key": "text",
+        "hook_key": "text",
+        "_sqlmesh__record_source": "text",
+        "_sqlmesh_extracted_at": "timestamp"
+    }
+)
+def execute(
+    context: ExecutionContext,
+    start: datetime,
+    end: datetime,
+    execution_time: datetime,
+    **kwargs: t.Any,
+) -> t.Generator[pd.DataFrame, None, None]:
+    """
+    Reads a YAML file, extracts the data under the 'keysets' key, and converts it into a table format.
+
+    :param yaml_file: Path to the YAML file.
+    :return: Pandas DataFrame containing the extracted data.
+    """
+    # Load the YAML file
+    relative_path = "../../../data/hook.yml"
+    script_dir = os.path.dirname(os.path.abspath(__file__))  # Directory of the current script
+    absolute_path = os.path.abspath(os.path.join(script_dir, relative_path))
+    
+    with open(absolute_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+    # Extract the 'keysets' data
+    keysets = data.get('keysets', [])
+
+    # Convert the list of dictionaries into a DataFrame
+    df = pd.DataFrame(keysets)
+    
+    df["_sqlmesh__record_source"] = relative_path
+    df["_sqlmesh_extracted_at"] = execution_time.replace(tzinfo=None)
+
+    yield df
---
+++ models/bronze/snapshot/snp__hook__keysets.sql
@@ -0,0 +1,18 @@
+/* Snapshot model of the keysets from the hook yaml */
+MODEL (
+  name bronze.snapshot.snp__hook__keysets,
+  kind SCD_TYPE_2_BY_COLUMN (
+    unique_key _sqlmesh_hash_diff,
+    valid_from_name _sqlmesh_valid_from,
+    valid_to_name _sqlmesh_valid_to,
+    columns [_sqlmesh_hash_diff]
+  )
+);
+
+SELECT
+  *,
+  @generate_surrogate_key__sha_256(
+    @star_v2(relation := raw__hook__keysets, exclude := _sqlmesh_extracted_at, select_only := TRUE)
+  ) AS _sqlmesh_hash_diff,
+  @execution_ts::TIMESTAMP AS _sqlmesh_loaded_at
+FROM bronze.raw.raw__hook__keysets
