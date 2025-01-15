76c48642ab4e08c6cbf0a0c2460c6da259db4d6f
Mattias Thalén <bitter-polders0x@icloud.com>
2025-01-15 11:31:23 +0100

    Update and rename snp__hook__common_business_terms.sql to raw__hook__business_concepts.sql

--- models/bronze/snapshot/raw__hook__business_concepts.sql
+++ models/bronze/snapshot/raw__hook__business_concepts.sql
@@ -1,6 +1,6 @@
-/* Snapshot model of the common business terms from the hook yaml */
+/* Snapshot model of the business concepts from the hook yaml */
 MODEL (
-  name bronze.snapshot.snp__hook__common_business_terms,
+  name bronze.snapshot.snp__hook__business_concepts,
   kind SCD_TYPE_2_BY_COLUMN (
     unique_key _sqlmesh_hash_diff,
     valid_from_name _sqlmesh_valid_from,
@@ -12,7 +12,7 @@ MODEL (
 SELECT
   *,
   @generate_surrogate_key__sha_256(
-    @star_v2(relation := raw__hook__keysets, exclude := _sqlmesh_extracted_at, select_only := TRUE)
+    @star_v2(relation := raw__hook__business_concepts, exclude := _sqlmesh_extracted_at, select_only := TRUE)
   ) AS _sqlmesh_hash_diff,
   @execution_ts::TIMESTAMP AS _sqlmesh_loaded_at
-FROM bronze.raw.raw__hook__common_business_terms
+FROM bronze.raw.raw__hook__business_concepts
