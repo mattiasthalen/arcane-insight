35193332a3e5383dc40c42e522addf46547184df
Mattias Thalén <bitter-polders0x@icloud.com>
2025-01-18 02:23:53 +0100

    Add cdc to pipeline

--- pipelines/battle_net.py
+++ pipelines/battle_net.py
@@ -1,10 +1,13 @@
 import dlt
 import os
 
-from dlt.common.pipeline import current_pipeline, resource_state
 import polars as pl
+import polars.selectors as cs
 
-#from dlt.common.schema.schema import is_new_table
+import dlt_utilities.cdc_strategy as cdc
+
+from dlt.common.pipeline import current_pipeline, resource_state
+from dlt.sources.helpers.transform import add_row_hash_to_table
 from dlt.sources.rest_api import RESTAPIConfig, rest_api_resources
 
 from typing import Any, Optional
@@ -12,115 +15,6 @@ from dotenv import load_dotenv
 
 def print_script_name() -> None:
     print(f"Running script: {os.path.basename(__file__)}")
-    
-def add_hash_to_rows(df: pl.DataFrame, label: str = "_dlt_cdc_hash") -> pl.DataFrame:
-    return df.with_columns(pl.struct(pl.all()).hash().alias(label))
-
-def collect_set(df: pl.DataFrame, column: str) -> set:
-    return set(df.select(column).to_series().to_list())
-
-def set_cdc_action(df: pl.DataFrame, action: str, label: str = "_dlt_cdc_action") -> pl.DataFrame:
-    return df.with_columns(_cdc_action=pl.lit(action))
-    
-def collect_rows_to_insert(
-    source_df: pl.DataFrame,
-    key_name: str,
-    source_keys: set,
-    destination_keys
-) -> pl.DataFrame:
-    
-    keys = source_keys-destination_keys
-    filter = pl.col(key_name).is_in(keys)
-    filtered_df = source_df.filter(filter)
-    df = set_cdc_action(filtered_df, "insert")
-    
-    return df
-    
-def collect_rows_to_reinsert(
-    source_df: pl.DataFrame,
-    destination_df: pl.DataFrame,
-    key_name: str,
-    cdc_hash_label: str = "_dlt_cdc_hash",
-    cdc_action_label: str = "_dlt_cdc_action"
-) -> pl.DataFrame:
-    
-    keys = collect_set(destination_df.filter(pl.col(cdc_action_label) == "delete"), key_name)
-    filter = pl.col(key_name).is_in(keys)
-    filtered_df = source_df.filter(filter)
-    df = set_cdc_action(filtered_df, "reinsert", cdc_action_label)
-    
-    return df
-
-def collect_rows_to_update(
-    source_df: pl.DataFrame,
-    key_name: str,
-    source_keys: set,
-    destination_keys: set,
-    source_hashes: set,
-    destination_hashes: set,
-    reinsert_hashes: set,
-    cdc_hash_label: str = "_dlt_cdc_hash",
-    cdc_action_label: str = "_dlt_cdc_action"
-) -> pl.DataFrame:
-    
-    updated_pks = source_keys.intersection(destination_keys)
-    updated_hashes = source_hashes - destination_hashes - reinsert_hashes
-    filter = pl.col(key_name).is_in(updated_pks) & pl.col(cdc_hash_label).is_in(updated_hashes)
-    filtered_df = source_df.filter(filter)
-    df = set_cdc_action(filtered_df, "update", cdc_action_label)
-    
-    return df
-
-def collect_rows_to_delete(
-    destination_df: pl.DataFrame,
-    key_name: str,
-    source_keys: set,
-    destination_keys,
-    cdc_action_label: str = "_dlt_cdc_action"
-) -> pl.DataFrame:
-    
-    deleted_pks = destination_keys-source_keys
-    filter = pl.col(key_name).is_in(deleted_pks) & ~(pl.col(cdc_action_label) == "delete")
-    filtered_df = destination_df.filter(filter)
-    df = set_cdc_action(filtered_df, "delete", cdc_action_label)
-    
-    return df
-    
-def generate_cdc_delta(
-    source_df: pl.DataFrame,
-    destination_df: pl.DataFrame,
-    primary_key: str = "id",
-    cdc_hash_label: str = "_dlt_cdc_hash",
-    cdc_action_label: str = "_dlt_cdc_action"
-) -> pl.DataFrame:
-    
-    # Add hash to source rows
-    hashed_source_df = add_hash_to_rows(source_df, cdc_hash_label)
-    
-    # Collect primary keys & hashes
-    source_pks = collect_set(hashed_source_df, primary_key)
-    destination_pks = collect_set(destination_df, primary_key)
-    source_hashes = collect_set(hashed_source_df, cdc_hash_label)
-    destination_hashes = collect_set(destination_df, cdc_hash_label)
-    
-    # Create delta dataframe
-    insert_df = collect_rows_to_insert(hashed_source_df, primary_key, source_pks, destination_pks)
-    reinsert_df = collect_rows_to_reinsert(hashed_source_df, destination_df, primary_key)
-    delete_df = collect_rows_to_delete(destination_df, primary_key, source_pks, destination_pks, cdc_action_label)
-    
-    update_df = collect_rows_to_update(
-        hashed_source_df,
-        primary_key,
-        source_pks,
-        destination_pks,
-        source_hashes,
-        destination_hashes,
-        collect_set(reinsert_df, cdc_hash_label)
-    )
-    
-    delta_df = pl.concat([insert_df, reinsert_df, update_df, delete_df])
-    
-    return delta_df
 
 @dlt.source(name="battle_net")
 def battle_net__source(credentials = dlt.secrets.value) -> Any:
@@ -136,6 +30,7 @@ def battle_net__source(credentials = dlt.secrets.value) -> Any:
 
         },
         "resource_defaults": {
+            "primary_key": "id",
             "write_disposition": "replace",
             "max_table_nesting": 0,
             "endpoint": {
@@ -147,7 +42,6 @@ def battle_net__source(credentials = dlt.secrets.value) -> Any:
         "resources": [
             {
                 "name": "raw__hearthstone__cards",
-                "primary_key": "id",
                 "endpoint": {
                     "path": "hearthstone/cards",
                     "params": {
@@ -160,111 +54,111 @@ def battle_net__source(credentials = dlt.secrets.value) -> Any:
                     },
                 },
             },
-            # {
-            #     "name": "raw__hearthstone__cardbacks",
-            #     "endpoint": {
-            #         "path": "hearthstone/cardbacks",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__metadata",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata",
-            #         "paginator": "single_page",
-            #     },
-            # },            
-            # {
-            #     "name": "raw__hearthstone__sets",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/sets",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__set_groups",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/setGroups",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__types",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/types",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__rarities",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/rarities",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__classes",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/classes",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__minion_types",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/minionTypes",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__keywords",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/keywords",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__game_modes",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/game_modes",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # # {
-            # #     "name": "raw__hearthstone__bg_game_modes",
-            # #     "endpoint": {
-            # #         "path": "hearthstone/metadata/bgGameModes",
-            # #         "paginator": "single_page",
-            # #     },
-            # # },
-            # {
-            #     "name": "raw__hearthstone__cardback_categories",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/cardBackCategories",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__mercenary_factions",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/mercenaryFactions",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__mercenary_roles",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/mercenaryRoles",
-            #         "paginator": "single_page",
-            #     },
-            # },
-            # {
-            #     "name": "raw__hearthstone__spell_schools",
-            #     "endpoint": {
-            #         "path": "hearthstone/metadata/spellSchools",
-            #         "paginator": "single_page",
-            #     },
-            # },
+            {
+                "name": "raw__hearthstone__cardbacks",
+                "endpoint": {
+                    "path": "hearthstone/cardbacks",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__metadata",
+                "endpoint": {
+                    "path": "hearthstone/metadata",
+                    "paginator": "single_page",
+                },
+            },            
+            {
+                "name": "raw__hearthstone__sets",
+                "endpoint": {
+                    "path": "hearthstone/metadata/sets",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__set_groups",
+                "endpoint": {
+                    "path": "hearthstone/metadata/setGroups",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__types",
+                "endpoint": {
+                    "path": "hearthstone/metadata/types",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__rarities",
+                "endpoint": {
+                    "path": "hearthstone/metadata/rarities",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__classes",
+                "endpoint": {
+                    "path": "hearthstone/metadata/classes",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__minion_types",
+                "endpoint": {
+                    "path": "hearthstone/metadata/minionTypes",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__keywords",
+                "endpoint": {
+                    "path": "hearthstone/metadata/keywords",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__game_modes",
+                "endpoint": {
+                    "path": "hearthstone/metadata/gameModes",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__bg_game_modes",
+                "endpoint": {
+                    "path": "hearthstone/metadata/bgGameModes",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__cardback_categories",
+                "endpoint": {
+                    "path": "hearthstone/metadata/cardBackCategories",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__mercenary_factions",
+                "endpoint": {
+                    "path": "hearthstone/metadata/mercenaryFactions",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__mercenary_roles",
+                "endpoint": {
+                    "path": "hearthstone/metadata/mercenaryRoles",
+                    "paginator": "single_page",
+                },
+            },
+            {
+                "name": "raw__hearthstone__spell_schools",
+                "endpoint": {
+                    "path": "hearthstone/metadata/spellSchools",
+                    "paginator": "single_page",
+                },
+            },
             
             
         ]
@@ -283,61 +177,47 @@ def battle_net__load() -> None:
         #dev_mode=True
     )
     
-    current_pipeline = dlt.current.pipeline()
-    
+    primary_key = "id"
     source = battle_net__source()
     resources = source.resources
     
-    source_datasets = pipeline.dataset()
-    destination_datasets = current_pipeline.dataset()
-    
-    cdc_hash_label = "_dlt_cdc_hash"
-    cdc_action_label = "_dlt_cdc_action"
-    
-    for resource in resources:
-        print(f"Processing resource: {resource}")
+    for resource in resources.items():
+        resource_name, resource_object = resource
+        print(f"\nProcessing resource: {resource_name}")
         
-        source_data = source_datasets[resource]
-        destination_data = destination_datasets[resource]
+        # Try and load the destination data
+        try:
+            destination_df = pl.DataFrame(pipeline.dataset()[resource_name].df())
+            destination_exists = True
         
-        source_df = pl.DataFrame(source_data.df())
-        destination_df = pl.DataFrame(destination_data.df())
+        except dlt.destinations.exceptions.DatabaseUndefinedRelation:
+            destination_df = pl.DataFrame()
+            destination_exists = False
         
-        print("Source data:")
-        print(source_df.head())
-        
-        print("Destination data:")
-        print(destination_df.head())
-        
-        if not pipeline.first_run:
-            print("First run")
-            hashed_source_df = add_hash_to_rows(source_df)
-            action_source_df = set_cdc_action(hashed_source_df, "insert", cdc_action_label)
+        if pipeline.first_run or not destination_exists:
+            print("Mode: Full load")
+            source_df = pl.DataFrame(resource_object)
+            load_df = cdc.add_hash_to_rows(source_df, source_df.columns)
+            
+        else:
+            print("Mode: CDC detection")
 
-            print("Action source data:")
-            print(action_source_df.head())
-            #print(action_source_df.select(["id", cdc_hash_label, cdc_action_label]).head())
-        
-        return None
-        
-        # Need flag to see if the resource has been loaded before
-        resource_state = dlt.current.resource_state()
-        
-        print(resource_state)
-        #print(destination_df.head())
-        
-        """
-        df = generate_cdc_delta(
-            source_df=source_df
-            destination_df=destination_df
-            primary_key=primary_key
-            cdc_hash_label=cdc_hash_label
-            cdc_action_label=cdc_action_label
-        )
-        
-        #resource_object = df
-        yield df
-        """
+            source_df = pl.DataFrame(resource_object)
+            source_df.columns = destination_df.columns
+            
+            load_df = cdc.extract_cdc_data(              
+                source_df=source_df,
+                target_df=destination_df,
+                key_columns=[primary_key],
+                order_by="_dlt_load_id",
+                detect_by=source_df.columns,
+                descending=True,
+                cdc_action_label="_dlt_cdc_action",
+                cdc_hash_label="_dlt_cdc_hash"
+            )
+
+        load_info = pipeline.run(load_df.to_arrow(), table_name=resource_name)
+        print(f"\n{load_info}")
     
 if __name__ == "__main__":
     battle_net__load()
